699150e760b461b52470f935f2d2156bcdfe362b
Tammy Wokoma <tamunosakiwokoma86@gmail.com>
2025-11-29 15:46:55 -0700

    Populate monitor_clipboard.py

--- monitor_clipboard.py
+++ monitor_clipboard.py
@@ -1,1 +1,26 @@
+import pyperclip
+import time
 
+def monitor_clipboard():
+    print('Recording clipboard... (Ctrl-C to stop)')
+    previous_content = ''
+    change_count = 0
+
+    try:
+        while True:
+            content = pyperclip.paste()
+            if content != previous_content:
+                change_count += 1
+                print(f"
+--- Clipboard Change #{change_count} ---")
+                print(content)
+                print("-" * 40)
+                previous_content = content
+            time.sleep(0.1)  # Reduced frequency to 0.1s to be less resource-intensive
+
+    except KeyboardInterrupt:
+        print(f"
+Stopped. Total clipboard changes recorded: {change_count}")
+
+if __name__ == "__main__":
+    monitor_clipboard()
